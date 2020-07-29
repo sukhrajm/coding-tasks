ecff7c095632c4fe0ff0f3be1d6905525c8e8800
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2020-07-29 23:45:36 +0100

    Implementation added to make test pass, added other tests

--- src/main/java/lessonone/BinaryGap.java
+++ src/main/java/lessonone/BinaryGap.java
@@ -1,8 +1,37 @@
 package lessonone;
 
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * Solution to the problem as described here:
+ * https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+ * @author Sukhraj Rattan
+ *
+ */
 public class BinaryGap {
 
     public int solution(int n) {
-        return -1;
+
+        String binaryString = Integer.toBinaryString(n);
+
+        int maxGap = 0;
+        StringTokenizer s = new StringTokenizer(binaryString, "1");
+
+        while(s.hasMoreTokens()) {
+
+            String gap = s.nextToken();
+
+            //if it was not tokenized, the token would be the same as original
+            if (gap.equals(binaryString)) {
+                return 0;
+            }
+
+            if (gap.length() > maxGap) {
+                maxGap = gap.length();
+            }
+        }
+
+        return maxGap;
     }
 }
--- src/test/java/lessonone/BinaryGapTest.java
+++ src/test/java/lessonone/BinaryGapTest.java
@@ -2,16 +2,49 @@ package lessonone;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class BinaryGapTest {
 
     @Test
-    public void testBinaryGap() {
+    public void testWithGaps() {
 
         BinaryGap binaryGap = new BinaryGap();
-        int result = binaryGap.solution(1041);
-        assertTrue( result == 5);
+        int result = binaryGap.solution(1041); //10000010001
+        assertEquals( 5, result);
+    }
+
+    @Test
+    public void testWithNoGaps() {
+
+        BinaryGap binaryGap = new BinaryGap();
+        int result = binaryGap.solution(2147483647); //1111111111111111111111111111111
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testWithManyGaps() {
+
+        BinaryGap binaryGap = new BinaryGap();
+        int result = binaryGap.solution(4356); //001000100000100
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testWithSingleZero() {
+
+        BinaryGap binaryGap = new BinaryGap();
+        int result = binaryGap.solution(0); //0
+        assertEquals(0, result);
+    }
+
+
+    @Test
+    public void testWithSingleOne() {
+
+        BinaryGap binaryGap = new BinaryGap();
+        int result = binaryGap.solution(1); //1
+        assertEquals(0, result);
     }
 }
 
