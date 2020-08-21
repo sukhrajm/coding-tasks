b71b07354d9f9914b6f464cdf7fa3aae7b6da3a7
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2020-08-21 22:11:45 +0100

    refactor to remove unneccessary checks

--- src/main/java/lessonone/BinaryGap2.java
+++ src/main/java/lessonone/BinaryGap2.java
@@ -10,39 +10,31 @@ public class BinaryGap2 {
 
     public int solution(int i) {
 
-        int numberOfZeros = 0;
+        int currentGapSize = 0;
         boolean hasSeenAOne = false;
         int maxGap = 0;
 
         while(i != 0) {
 
-            if (i <= 1 ) {
-                return maxGap;
-            }
-
             //get the remainder after halving i.e look at the right most digit
             int remainder = i % 2;
             if (remainder == 0) {
                 if (hasSeenAOne) {
-                    numberOfZeros++;
-                    maxGap = getGap(numberOfZeros, maxGap);
+                    currentGapSize++;
+                    maxGap = getMaximumGap(currentGapSize, maxGap);
 
                 }
 
             } else {
-                if (hasSeenAOne) {
-                    //if already seen one
-                    maxGap = getGap(numberOfZeros, maxGap);
-                    numberOfZeros = 0;
-                }
+                currentGapSize = 0;
                 hasSeenAOne = true;
             }
             i = i >> 1;
         }
-        return 0;
+        return maxGap;
     }
 
-    private int getGap(int numberOfZeros, int maxGap) {
+    private int getMaximumGap(int numberOfZeros, int maxGap) {
         if (numberOfZeros > maxGap) {
             return numberOfZeros;
         }
