f7d81c63f886f71e5e559b10190671a181f4f659
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2020-09-09 21:37:34 +0100

    make the previous failing test pass, commit next failing test

--- src/main/java/lessontwo/CyclicRotation.java
+++ src/main/java/lessontwo/CyclicRotation.java
@@ -9,11 +9,30 @@ public class CyclicRotation {
 
     public int[] solution(int[] input, int numberOfCycles) {
 
-        if (input.length > 1) {
+        if (input.length ==2 && numberOfCycles == 1) {
             int firstItem = input[0];
             input[0] = input[1];
             input[1] = firstItem;
+        } else {
+            if (numberOfCycles == 1){
+                int[] transformedInput = new int[input.length];
+                int newArrayIndex = 0;
+
+                for (int i=0; i<input.length-1; i++) {
+                    transformedInput[newArrayIndex + 1] = input[i];
+                    newArrayIndex++;
+                    if (newArrayIndex == input.length - 1) {
+                        transformedInput[0] = input[input.length - 1];
+                    }
+
+                }
+                input = transformedInput;
+
+            }
+
         }
+
+
         return input;
     }
 }
--- src/test/java/lessontwo/CyclicRotationTest.java
+++ src/test/java/lessontwo/CyclicRotationTest.java
@@ -4,7 +4,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class CyclicRotationTest {
 
@@ -53,6 +52,20 @@ public class CyclicRotationTest {
 
         //then
         int[] expected = new int[]{7, 2, 4};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testThreeItems_WithMoreThanOneCycle() {
+        //given
+        int input[] = new int[]{2, 4, 7};
+        int numberOfTurns = 2;
+
+        //when
+        int[] result = cyclicRotation.solution(input, numberOfTurns);
+
+        //then
+        int[] expected = new int[]{4, 7, 2};
         assertArrayEquals(expected, result);
     }
 }
