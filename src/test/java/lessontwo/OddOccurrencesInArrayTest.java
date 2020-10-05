749ccf97eaa9361d1fc7237daee9ea308bf3ea34
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2020-10-05 20:49:18 +0100

    made the first test pass

--- src/test/java/lessontwo/OddOccurrencesInArrayTest.java
+++ src/test/java/lessontwo/OddOccurrencesInArrayTest.java
@@ -2,8 +2,18 @@ package lessontwo;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Solution for exercise: https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+ *
+ * @author sukhraj rattan
+ */
 public class OddOccurrencesInArrayTest {
 
     @Test
@@ -12,14 +22,28 @@ public class OddOccurrencesInArrayTest {
         int[] array = new int[]{2,2,3};
 
         //when
-        int result = findOddOccurance(array);
+        int result = findOddOccurrence(array);
 
         //then
         assertEquals(3, result);
 
     }
 
-    private int findOddOccurance(int[] array) {
-        return -1;
+    private int findOddOccurrence(int[] array) {
+        List<Integer> unpaired = new ArrayList<>();
+
+        for (int i=0; i<array.length; i++) {
+            int current = array[i];
+            if (unpaired.contains(current)) {
+                //if already in the list, then it's paired so remove
+                unpaired.remove((Integer)current);
+
+            } else {
+                unpaired.add(current);
+            }
+        }
+
+        return unpaired.get(0);
+
     }
 }
