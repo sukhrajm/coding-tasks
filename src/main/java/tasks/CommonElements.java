cb4c8d998821a3207e6177335aef3e85c8ed005b
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2020-11-26 22:28:22 +0000

    added solution by solving with large array

--- src/main/java/tasks/CommonElements.java
+++ src/main/java/tasks/CommonElements.java
@@ -6,4 +6,45 @@ package tasks;
 public class CommonElements {
 
 
+    public int find(int[] input1, int[] input2) {
+
+        //input1 [2,6,7,8,9,12,13,16,19,21,24,26,29]
+
+        //input2 [6,8,9,11,13,16,19,22,23,24,31,34]
+
+        //iterate the biggest array
+        int largestLength = input1.length > input1.length ? input1.length : input2.length;
+
+        int numberOfCommonItems = 0;
+
+        int indexOfArrayOne = 0;
+
+        int indexOfArrayTwo = 0;
+
+
+        while(indexOfArrayOne < largestLength) {
+
+            int itemInThisArray = input1[indexOfArrayOne];
+
+            int itemInOtherArray = input2[indexOfArrayTwo];
+
+            if (itemInThisArray < itemInOtherArray) {
+                //then it cant be in the other array, so move on
+                indexOfArrayOne++;
+            } else if (itemInThisArray == itemInOtherArray) {
+                numberOfCommonItems++;
+                indexOfArrayOne++;
+                indexOfArrayTwo++;
+
+            } else {
+                //this item is less than the other one
+                indexOfArrayTwo++;
+            }
+
+        }
+
+        return numberOfCommonItems;
+
+    }
+
 }
--- src/test/java/tasks/CommonElementsTest.java
+++ src/test/java/tasks/CommonElementsTest.java
@@ -22,6 +22,25 @@ public class CommonElementsTest {
         int result = commonElements.find(input1, input2);
         assertEquals(1, result);
     }
+
+    @Test
+    public void findCommon_withVeryLargeArrays(){
+        int[] input1 = new int[]{2,6,7,8,9,12,13,16,19,21,24,26,29};
+        int[] input2 = new int[]{6,8,9,11,13,16,19,22,23,24,31,34};
+
+        int result = commonElements.find(input1, input2);
+        assertEquals(7, result);
+    }
+
+    //fails with index out of bounds
+    @Test
+    public void findCommon_withFirstArraySmaller(){
+        int[] input1 = new int[]{2,6,7,8,9,12,13,16,19,21,24};
+        int[] input2 = new int[]{6,8,9,11,13,16,19,22,23,24,31,34};
+
+        int result = commonElements.find(input1, input2);
+        assertEquals(6, result);
+    }
 }
 
 
