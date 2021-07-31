ce39b8f85e59b25bdad1e05cbe1f1a7df95ddebd
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2021-07-31 22:32:15 +0100

    new impl added

--- src/test/java/lessonthree/PermMissingElementTest.java
+++ src/test/java/lessonthree/PermMissingElementTest.java
@@ -2,29 +2,42 @@ package lessonthree;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
+
+/**
+ ▶large_range
+ range sequence, length = ~100,000✘WRONG ANSWER
+ got -2147483647 expected 1
+ 1.0.244 sOK
+ 2.0.124 sWRONG ANSWER, got -2147483647 expected 1
+ 3.0.120 sWRONG ANSWER, got -2147471303 expected 12345
+
+ large2
+ large test, length = ~100,000✘WRONG ANSWER
+ got -2147473647 expected 10001
+ 1.0.144 sWRONG ANSWER, got -2147473647 expected 10001
+
+ */
+
 
 public class PermMissingElementTest {
 
     public int solution(int[] A) {
-        if (A.length == 1) {
-            return 0;
+        if (A.length == 0) {
+            return 1;
         }
-        Arrays.sort(A);
-        int result = -1;
+        int result = 0;
 
-        for (int i=0; i<A.length; i++) {
-            int current = A[i];
-            int next = -1;
-            if (i != A.length-1) {
-                next = A[i+1];
-            }
-            if (next - current != 1 && next != -1) {
-                result = next - 1;
-            }
+        int maximumNumberInArray = A.length + 1;
+
+        int totalSumOfArray = (maximumNumberInArray * (maximumNumberInArray + 1)) / 2;
+
+        int runningTotal = 0;
+        for(int i = 0; i < A.length; i++) {
+            runningTotal = runningTotal + A[i];
         }
+
+        result = totalSumOfArray - runningTotal;
         return result;
     }
 
@@ -33,7 +46,7 @@ public class PermMissingElementTest {
         int[] a = new int[]{1};
 
         int result = solution(a);
-        assertEquals(0, result);
+        assertEquals(2, result);
     }
 
     @Test
@@ -42,5 +55,29 @@ public class PermMissingElementTest {
 
         int result = solution(a);
         assertEquals(3, result);
+    }
+
+    @Test
+    public void testWithEmptyList() {
+        int[] a = new int[]{};
+
+        int result = solution(a);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testWithSingleElement() {
+        int[] a = new int[]{1};
+
+        int result = solution(a);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testWithTwoElements() {
+        int[] a = new int[]{1,3};
+
+        int result = solution(a);
+        assertEquals(2, result);
     }
 }
