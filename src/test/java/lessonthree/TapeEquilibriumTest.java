a3d5b6b46760c4649db888cc86414781e32c2cd3
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2021-08-01 18:07:44 +0100

    add failing test

--- src/test/java/lessonthree/TapeEquilibriumTest.java
+++ src/test/java/lessonthree/TapeEquilibriumTest.java
@@ -12,13 +12,16 @@ public class TapeEquilibriumTest {
             return A[0];
         }
 
+        if (A.length == 2) {
+            return A[1] - A[0];
+        }
+
         return -1;
     }
 
     @Test
     public void testWithOneElementInArray() {
         int[] a = new int[]{3};
-        int p = 0;
         int result = solution(a);
         assertEquals(3, result);
     }
@@ -26,11 +29,18 @@ public class TapeEquilibriumTest {
     @Test
     public void testWithTwoElementsInArray() {
         int[] a = new int[]{1, 2};
-
-        int p = 1;
         int result = solution(a);
         assertEquals(1, result);
+    }
 
+    @Test
+    public void testWithThreeElementsInArrayOneSplit() {
+        int[] a = new int[]{3, 1, 9};
 
+        //4,9 = 5
+        //3, 10 = 7
+
+        int result = solution(a);
+        assertEquals(5, result);
     }
 }
