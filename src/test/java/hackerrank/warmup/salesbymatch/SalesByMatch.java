38f057fe9694dd494902d3255c67a215125f8cac
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2022-02-01 11:50:54 +0000

    skeleton code

---
+++ src/test/java/hackerrank/warmup/salesbymatch/SalesByMatch.java
@@ -0,0 +1,17 @@
+package hackerrank.warmup.salesbymatch;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class SalesByMatch {
+
+
+
+
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+        return 0;
+    }
+}
