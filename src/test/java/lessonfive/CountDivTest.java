7cefdb4884f98cc4e3597ce5542f64911646bed3
uk45002334 <sukhraj.matharu@tesco.com>
2024-10-12 20:09:10 +0100

    add task for count div

--- src/test/java/lessonfive/CountDivTest.java
+++ src/test/java/lessonfive/CountDivTest.java
@@ -8,8 +8,15 @@ public class CountDivTest {
 
   public int solution(int A, int B, int K) {
 
+    if (A == K && B < (A + K)) {
+      return 1;
+    }
 
     double val = (double)(B - A) / K;
+    if (val < 1 && A != K ) {
+      return 0;
+    }
+
     double res = Math.ceil(val);
     if (A == K) {
       res = res + 1;
@@ -46,6 +53,26 @@ public class CountDivTest {
 
     int result = solution(a, b, k);
     assertEquals(7, result);
+  }
+
+  @Test
+  public void testSolutionWith0Result() {
+    int a = 3;
+    int b = 4;
+    int k = 2;
+
+    int result = solution(a, b, k);
+    assertEquals(0, result);
+  }
+
+  @Test
+  public void testSolutionWithOneResult() {
+    int a = 3;
+    int b = 5;
+    int k = 3;
+
+    int result = solution(a, b, k);
+    assertEquals(1, result);
   }
 
 }
